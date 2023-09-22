16dcdaa2a5acb71af1e9983b5057915c91718783
cherubinof <tiossepcherubin@gmail.com>
2023-09-22 12:26:29 +0200

    style2

--- registration/forms.py
+++ registration/forms.py
@@ -1,9 +1,50 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from django import forms
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm, UsernameField, PasswordChangeForm,PasswordResetForm,SetPasswordForm
+from django.contrib.auth.models import User
+from django.forms import fields, widgets
+from django.utils.translation import gettext,gettext_lazy as _
+from django.contrib.auth import password_validation
+from app_client.models import Client
+from app_produit.models import Produit
+from django .forms import ModelForm
+
 
 
 class InscriptionForm(UserCreationForm):
+    password1=forms.CharField(label='password',widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2=forms.CharField(label='confirm password',widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    email=forms.CharField(required=True,widget=forms.EmailInput(attrs={'class':'form-control'}))
     class Meta:
-        model = User
-        fields = ['username','email','password1','password2']
+        model=User
+        fields=['username','email','password1','password2']
+        labels={'email':'Email'}
+        widgets={'username':forms.TextInput(attrs={'class':'form-control'})}
+
+
+
+class LoginForm(AuthenticationForm):
+    username=UsernameField(widget=forms.TextInput(attrs={'autofocus':True,'class':'form-control'}))
+    password=forms.CharField(label= _("password"),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'current-password','class':'form-control'}))
+
+
+class MyPasswordChangeForm(PasswordChangeForm):
+    old_password=forms.CharField(label=_("old password"),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'current-password','autofocus':True,'class':'form-control'}))
+    new_password1=forms.CharField(label=_("new password"),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new-password','class':'form-control'}),help_text=password_validation.password_validators_help_text_html())
+    new_password2=forms.CharField(label=_("confirm new password"),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new-password','class':'form-control'}))
+
+
+class MyPasswordResetForm(PasswordResetForm):
+    email=forms.EmailField(label=_("Email"),max_length=254,widget=forms.EmailInput(attrs={'autocomplete':'email','class':'form-control'}))
+
+
+
+class MySetPasswordForm(SetPasswordForm):
+    new_password1=forms.CharField(label=_("new password"),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new-password','class':'form-control'}),help_text=password_validation.password_validators_help_text_html())
+    new_password2=forms.CharField(label=_("confirm new password"),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new-password','class':'form-control'}))
+
+
+
+
+
+
+
--- registration/urls.py
+++ registration/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from registration import views
 from django.contrib.auth import views as auth_views
+from .forms import LoginForm
 
 
 urlpatterns = [
     path('register',views.register,name='register'),
-    path('login',auth_views.LoginView.as_view(template_name='registration/login.html'),name='connexion'),
+    path('login',auth_views.LoginView.as_view(template_name='registration/login.html',authentication_form=LoginForm),name='connexion'),
     path('logout',auth_views.LogoutView.as_view(template_name='registration/logout.html'),name='logout'),
 ]
