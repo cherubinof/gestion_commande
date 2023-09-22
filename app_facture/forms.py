736f7db2ddd2789b1075812c7ae518b12121b139
cherubinof <tiossepcherubin@gmail.com>
2023-09-22 11:37:53 +0200

    style

--- app_facture/forms.py
+++ app_facture/forms.py
@@ -1,10 +1,19 @@
 from django.forms import ModelForm
 from django import forms
 from.models import Facture
+from django.forms import fields, widgets
+from django.utils.translation import gettext,gettext_lazy as _
 
 
 
 class FactureForm(ModelForm):
+   
+    
+    commentaire=forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}))
     class Meta:
         model = Facture
         fields = '__all__'
+        
+        
+       
+    
